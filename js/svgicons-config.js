d561a550013cf7fdafcb2251144decab353cb46c
Kristof Houben <kristof@treeshadow.be>
2013-11-16 01:22:24 +0100

    added gestures icon

--- js/svgicons-config.js
+++ js/svgicons-config.js
@@ -24,5 +24,71 @@ var svgIconConfig = {
 				} 
 			}
 		]
+	},
+	maximize : { 
+		url : 'svg/maximize.svg',
+		animation : [
+			{ 
+				el : 'path:nth-child(1)', 
+				animProperties : { 
+					from : { val : '{"transform" : "t0 0"}' }, 
+					to : { val : '{"transform" : "t-5 -5"}' }
+				} 
+			},
+			{ 
+				el : 'path:nth-child(2)', 
+				animProperties : { 
+					from : { val : '{"transform" : "t0 0"}' }, 
+					to : { val : '{"transform" : "t5 -5"}' }
+				} 
+			},
+			{ 
+				el : 'path:nth-child(3)', 
+				animProperties : { 
+					from : { val : '{"transform" : "t0 0"}' }, 
+					to : { val : '{"transform" : "t-5 5"}' }
+				} 
+			},
+			{ 
+				el : 'path:nth-child(4)', 
+				animProperties : { 
+					from : { val : '{"transform" : "t0 0"}' }, 
+					to : { val : '{"transform" : "t5 5"}' }
+				} 
+			}
+		]
+	},
+	maximizeRotate : { 
+		url : 'svg/maximize.svg',
+		animation : [
+			{ 
+				el : 'path:nth-child(1)', 
+				animProperties : { 
+					from : { val : '{"transform" : "r0 16 16 t0 0"}' }, 
+					to : { val : '{"transform" : "r180 16 16 t-5 -5"}' }
+				} 
+			},
+			{ 
+				el : 'path:nth-child(2)', 
+				animProperties : { 
+					from : { val : '{"transform" : "r0 48 16 t0 0"}' }, 
+					to : { val : '{"transform" : "r-180 48 16 t5 -5"}' }
+				} 
+			},
+			{ 
+				el : 'path:nth-child(3)', 
+				animProperties : { 
+					from : { val : '{"transform" : "r0 16 48 t0 0"}' }, 
+					to : { val : '{"transform" : "r-180 16 48 t-5 5"}' }
+				} 
+			},
+			{ 
+				el : 'path:nth-child(4)', 
+				animProperties : { 
+					from : { val : '{"transform" : "r0 48 48 t0 0"}' }, 
+					to : { val : '{"transform" : "r180 48 48 t5 5"}' }
+				} 
+			}
+		]
 	}
 };
